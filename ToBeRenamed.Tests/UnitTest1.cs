38a4a73eace99bf55d0b65d6ceb243fe76606577
Kyle Jones <mynameiskyle242@gmail.com>
2018-10-08 18:25:01 -0400

    Insert new Library using newly inserted user
    Library successfully gets inserted into database

--- ToBeRenamed.Tests/UnitTest1.cs
+++ ToBeRenamed.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using ToBeRenamed.Dtos;
@@ -27,11 +29,26 @@ namespace ToBeRenamed.Tests
             const string selectUserIdSql = @"
                 SELECT id FROM plum.users WHERE display_name = 'testUser'";
 
+            IEnumerable<UserDto> results;
+            
             using (var conn = connFactory.GetSqlConnection())
             {
                 // Insert new user, then get the user id
                 conn.Execute(insertUserSql);
-                var results = conn.Query<UserDto>(selectUserIdSql); // TODO - Replace UserDto with something in the ToBeRenamed.Tests namespace
+                results = conn.Query<UserDto>(selectUserIdSql); // TODO - Replace UserDto with something in the ToBeRenamed.Tests namespace
+            }
+
+            var userId = results.First().Id;
+            string title = "xUnitTitle";
+            var description = "xUnitDesc";
+            
+            const string insertLibrarySql = @"
+                INSERT INTO plum.libraries (title, description, created_by)
+                VALUES (@title, @description, @userId)";
+            
+            using (var cnn = connFactory.GetSqlConnection())
+            {
+                cnn.Execute(insertLibrarySql, new { userId, title, description });
             }
         }
     }
