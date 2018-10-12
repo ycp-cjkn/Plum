4524249ed6fc9e79ee30578c0db8e0ee8ff3add7
Kyle Jones <mynameiskyle242@gmail.com>
2018-10-11 20:29:15 -0400

    Change sql to add user to memberships table when they create library

--- ToBeRenamed/Commands/CreateLibraryHandler.cs
+++ ToBeRenamed/Commands/CreateLibraryHandler.cs
@@ -22,8 +22,14 @@ namespace ToBeRenamed.Commands
             var description = request.Description;
 
             const string sql = @"
-                INSERT INTO plum.libraries (title, description, created_by)
-                VALUES (@title, @description, @userId)";
+                WITH libraries AS (
+                    INSERT INTO plum.libraries (title, description, created_by)
+                    VALUES (@title, @description, @userId)
+                    RETURNING id, created_by
+                )
+                INSERT INTO plum.memberships (library_id, user_id, display_name)
+                SELECT libraries.id, created_by, display_name FROM libraries
+                INNER JOIN plum.users ON users.id = libraries.created_by;";
 
             using (var cnn = _sqlConnectionFactory.GetSqlConnection())
             {
