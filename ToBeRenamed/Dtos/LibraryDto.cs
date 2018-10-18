58570832c6c3e43cdfe871fc0e6065a1355697d8
Jason Lefever <jlefever@ycp.edu>
2018-10-18 02:48:37 -0400

    Add members to library page. Redesign library page.

--- ToBeRenamed/Dtos/LibraryDto.cs
+++ ToBeRenamed/Dtos/LibraryDto.cs
@@ -4,7 +4,7 @@ namespace ToBeRenamed.Dtos
 {
     public class LibraryDto
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int CreatedBy { get; set; }
---
+++ ToBeRenamed/Queries/GetMembersOfLibrary.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using ToBeRenamed.Dtos;
+
+namespace ToBeRenamed.Queries
+{
+    public class GetMembersOfLibrary : IRequest<IEnumerable<MemberDto>>
+    {
+        public int LibraryId { get;}
+
+        public GetMembersOfLibrary(int libraryId)
+        {
+            LibraryId = libraryId;
+        }
+    }
+}
