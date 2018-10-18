1ebce7160d8e96c2fa2e9533c27a8a4f79968e9f
Jason Lefever <jlefever@ycp.edu>
2018-10-17 21:54:29 -0400

    Add CreateUserWithoutAuth command with a unit test

---
+++ ToBeRenamed.Tests/Commands/CreateUserWithoutAuthTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ToBeRenamed.Commands;
+using Xunit;
+
+namespace ToBeRenamed.Tests.Commands
+{
+    public class CreateUserWithoutAuthTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _fixture;
+
+        public CreateUserWithoutAuthTests(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task ItRuns()
+        {
+            _fixture.ResetDatabase();
+
+            var request = new CreateUserWithoutAuth("Alice");
+            var result = await _fixture.SendAsync(request);
+
+            Assert.NotNull(result);
+            Assert.Equal("Alice", result.DisplayName);
+            Assert.Null(result.GoogleClaimNameIdentifier);
+        }
+    }
+}
---
+++ ToBeRenamed/Commands/CreateUserWithoutAuth.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ToBeRenamed.Dtos;
+
+namespace ToBeRenamed.Commands
+{
+    public class CreateUserWithoutAuth : IRequest<UserDto>
+    {
+        public string DisplayName { get; }
+
+        public CreateUserWithoutAuth(string displayName)
+        {
+            DisplayName = displayName;
+        }
+    }
+}
