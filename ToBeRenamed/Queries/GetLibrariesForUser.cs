363fc6ab287ad59fea34d26ba28f3d3b3f51d4dd
Jason Lefever <jlefever@ycp.edu>
2018-10-18 03:34:39 -0400

    Display libraries where user is a member not just libraries the user created

---
+++ ToBeRenamed/Queries/GetLibrariesForUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using ToBeRenamed.Dtos;
+
+namespace ToBeRenamed.Queries
+{
+    public class GetLibrariesForUser : IRequest<IEnumerable<LibraryDto>>
+    {
+        public int UserId { get; }
+
+        public GetLibrariesForUser(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
---
+++ ToBeRenamed/Queries/GetLibrariesForUserHandler.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ToBeRenamed.Dtos;
+using ToBeRenamed.Factories;
+
+namespace ToBeRenamed.Queries
+{
+    public class GetLibrariesForUserHandler : IRequestHandler<GetLibrariesForUser, IEnumerable<LibraryDto>>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public GetLibrariesForUserHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task<IEnumerable<LibraryDto>> Handle(GetLibrariesForUser request, CancellationToken cancellationToken)
+        {
+            const string sql = @"
+                SELECT lib.id, lib.title, lib.description, lib.created_by, lib.created_at
+                FROM plum.libraries lib
+                INNER JOIN plum.memberships mem
+                ON lib.id = mem.library_id
+                WHERE mem.user_id = @UserId";
+
+            using (var cnn = _sqlConnectionFactory.GetSqlConnection())
+            {
+                return await cnn.QueryAsync<LibraryDto>(sql, new { request.UserId });
+            }
+        }
+    }
+}
