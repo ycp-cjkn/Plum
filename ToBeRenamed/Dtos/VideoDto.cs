51fd07e01607e532b9f895f25e9d9d1201e0246c
Kyle Jones <mynameiskyle242@gmail.com>
2018-10-20 15:38:47 -0400

    Remove unused VideoDto properties

--- ToBeRenamed/Dtos/VideoDto.cs
+++ ToBeRenamed/Dtos/VideoDto.cs
@@ -4,11 +4,7 @@ namespace ToBeRenamed.Dtos
 {
     public class VideoDto
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int VideoUrlId { get; set; }
-        public int LibraryId { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
