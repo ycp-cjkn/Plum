a7272eb8c141dae4b5e635710ebd4ffeb487e782
Nathaniel Russell <nrussell@ycp.edu>
2018-10-20 15:01:42 -0400

    Pull profile info queried from database and display on profile page

--- ToBeRenamed/Pages/Profile.cshtml.cs
+++ ToBeRenamed/Pages/Profile.cshtml.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToBeRenamed.Dtos;
+using ToBeRenamed.Queries;
 
 namespace ToBeRenamed.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IMediator _mediator;
+        public UserDto UserDto { get; set; }
 
+        public ProfileModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                UserDto = await _mediator.Send(new GetSignedInUserDto(User));
+            }
+            else
+            {
+                UserDto = await _mediator.Send(new GetUserByUserId(id.Value));
+            }
         }
     }
 }
---
+++ ToBeRenamed/Queries/GetUserByUserId.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using MediatR;
+using ToBeRenamed.Dtos;
+
+namespace ToBeRenamed.Queries
+{
+    public class GetUserByUserId : IRequest<UserDto>
+    {
+        public int UserId { get; }
+
+        public GetUserByUserId(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
