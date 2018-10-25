21b82ada7847eae4932087617a2a3800d953b564
Kyle Jones <mynameiskyle242@gmail.com>
2018-10-24 23:39:19 -0400

    Add Url to VideoDto

--- ToBeRenamed/Dtos/VideoDto.cs
+++ ToBeRenamed/Dtos/VideoDto.cs
@@ -7,5 +7,6 @@ namespace ToBeRenamed.Dtos
         public string Title { get; set; }
         public string Description { get; set; }
         public int Id { get; set; }
+        public string Url { get; set; }
     }
 }
