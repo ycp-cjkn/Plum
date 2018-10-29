83a77e2cee3ab413c88360b5bc963ad174d768cb
Nathaniel Russell <nrussell@ycp.edu>
2018-10-28 20:35:25 -0400

    Add placeholder info and forms to view and model, add edit link to library

--- ToBeRenamed/Pages/Libraries/Edit.cshtml.cs
+++ ToBeRenamed/Pages/Libraries/Edit.cshtml.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ToBeRenamed.Pages.Libraries
 {
+    [Authorize]
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IMediator _mediator;
 
+        public EditModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [BindProperty]
+        [Required]
+        public string Title { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Description { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+
+            return RedirectToPage("/Libraries");
         }
     }
 }
