e5db096605014ce048aab784767f9fb74d07bcf6
Jason Lefever <jlefever@ycp.edu>
2018-11-05 00:38:40 -0500

    Add initial roles page

---
+++ ToBeRenamed/Pages/Roles/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToBeRenamed.Dtos;
+using ToBeRenamed.Models;
+using ToBeRenamed.Queries;
+
+namespace ToBeRenamed.Pages.Roles
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IMediator _mediator;
+
+        public IndexModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IEnumerable<Role> Roles;
+
+        public async Task OnGetAsync(int libraryId)
+        {
+            Roles = await _mediator.Send(new GetRolesForLibrary(libraryId));
+        }
+    }
+}
