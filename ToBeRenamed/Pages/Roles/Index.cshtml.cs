b84d4a8b2bc3f0f82bd468037a2c8af5f9ebc0e7
Jason Lefever <jlefever@ycp.edu>
2018-11-06 01:13:17 -0500

    Update Roles page to show members and to allow updates to privileges

--- ToBeRenamed/Pages/Roles/Index.cshtml.cs
+++ ToBeRenamed/Pages/Roles/Index.cshtml.cs
@@ -1,7 +1,10 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ToBeRenamed.Commands;
 using ToBeRenamed.Dtos;
 using ToBeRenamed.Models;
 using ToBeRenamed.Queries;
@@ -10,18 +13,33 @@ namespace ToBeRenamed.Pages.Roles
 {
     public class IndexModel : PageModel
     {
+        private readonly IMapper _mapper;
         private readonly IMediator _mediator;
 
-        public IndexModel(IMediator mediator)
+        public IndexModel(IMapper mapper, IMediator mediator)
         {
+            _mapper = mapper;
             _mediator = mediator;
         }
 
+        public IEnumerable<MemberDto> Members;
+        public LibraryDto Library;
         public IEnumerable<Role> Roles;
 
         public async Task OnGetAsync(int libraryId)
         {
+            Members = await _mediator.Send(new GetMembersOfLibrary(libraryId));
+            Library = await _mediator.Send(new GetLibraryDtoById(libraryId));
             Roles = await _mediator.Send(new GetRolesForLibrary(libraryId));
+        }
+
+        public async Task<IActionResult> OnPostAsync(int roleId, string[] privileges)
+        {
+            var set = _mapper.Map<ISet<Privilege>>(privileges);
+
+            await _mediator.Send(new ReplacePrivilegesOfRole(roleId, set));
+
+            return RedirectToPage();
         }
     }
 }
