9867a342e60b24f3a670c6d854e0f38aac7a4f09
Nate <nrussell@ycp.edu>
2018-11-10 15:59:43 -0500

    Create commands and handlers for deleting videos

---
+++ ToBeRenamed/Commands/DeleteVideoFromLibrary.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace ToBeRenamed.Commands
+{
+    public class DeleteVideoFromLibrary : IRequest
+    {
+        public int DeletedAt;
+        public int VideoId;
+
+        public DeleteVideoFromLibrary(int deletedAt, int videoId)
+        {
+            DeletedAt = deletedAt;
+            VideoId = videoId;
+        }
+    }
+}
---
+++ ToBeRenamed/Commands/DeleteVideoFromLibraryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using ToBeRenamed.Factories;
+
+namespace ToBeRenamed.Commands
+{
+    public class DeleteVideoFromLibraryHandler : IRequestHandler<DeleteVideoFromLibrary>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public DeleteVideoFromLibraryHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task<Unit> Handle(DeleteVideoFromLibrary request, CancellationToken cancellationToken)
+        {
+            var deletedAt = request.DeletedAt;
+            var videoId = request.VideoId;
+
+            const string sql = @"
+                UPDATE plum.videos
+                SET deleted_at = @deletedAt
+                WHERE video_id = @videoId";
+
+            using (var cnn = _sqlConnectionFactory.GetSqlConnection())
+            {
+                await cnn.QueryAsync(sql, new { deletedAt, videoId });
+            }
+
+            return Unit.Value;
+        }
+    }
+}
