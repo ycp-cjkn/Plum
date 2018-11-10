ca695a2c4d7a3822c0bc6f4118c4805fac0ae54f
Jason Lefever <jlefever@ycp.edu>
2018-11-10 17:21:15 -0500

    Add Privilege checks for editing a library

--- ToBeRenamed/Models/Privilege.cs
+++ ToBeRenamed/Models/Privilege.cs
@@ -8,6 +8,7 @@ namespace ToBeRenamed.Models
         public static Privilege CanSubmitVideo = new Privilege("can_submit_video");
         public static Privilege CanRemoveOwnVideo = new Privilege("can_remove_own_video");
         public static Privilege CanRemoveAnyVideo = new Privilege("can_remove_any_video");
+        public static Privilege CanEditLibrary = new Privilege("can_edit_library");
 
         public static ISet<Privilege> All()
         {
@@ -15,7 +16,8 @@ namespace ToBeRenamed.Models
             {
                 CanSubmitVideo,
                 CanRemoveOwnVideo,
-                CanRemoveAnyVideo
+                CanRemoveAnyVideo,
+                CanEditLibrary,
             }.ToHashSet();
         }
 
