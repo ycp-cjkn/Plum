20a2b590ff5355ba9ecb562283125800514feb6d
Nathaniel Russell <nrussell@ycp.edu>
2018-11-18 19:14:06 -0500

    Fixed SignedInMemberHandler bug

--- ToBeRenamed/Queries/GetSignedInMemberHandler.cs
+++ ToBeRenamed/Queries/GetSignedInMemberHandler.cs
@@ -53,11 +53,13 @@ namespace ToBeRenamed.Queries
                 ON usr.id = mem.user_id
                 INNER JOIN plum.libraries lib
                 ON lib.id = mem.library_id
-                WHERE usr.google_claim_nameidentifier = @nameIdentifier";
+                WHERE
+                    usr.google_claim_nameidentifier = @nameIdentifier
+                    AND mem.library_id = @LibraryId";
 
             using (var cnn = _sqlConnectionFactory.GetSqlConnection())
             {
-                var dtos = await cnn.QueryAsync<MemberDto>(sql, new { nameIdentifier });
+                var dtos = await cnn.QueryAsync<MemberDto>(sql, new { nameIdentifier, request.LibraryId });
                 return _mapper.Map<Member>(dtos.Single());
             }
         }
