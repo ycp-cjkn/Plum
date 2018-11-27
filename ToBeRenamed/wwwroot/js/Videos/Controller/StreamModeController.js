9122dfafaac30541e26bae77b22eb798a9420d78
Kyle Jones <mynameiskyle242@gmail.com>
2018-11-27 18:56:24 -0500

    Implement stream mode
    Annotations appear in the annotations area when the video gets to their
    timestamp

--- ToBeRenamed/wwwroot/js/Videos/Controller/StreamModeController.js
+++ ToBeRenamed/wwwroot/js/Videos/Controller/StreamModeController.js
@@ -2,13 +2,11 @@ import { elements as changeModeElements } from '../View/StreamModeBase.js';
 import * as videoView from '../View/VideoView.js';
 import * as videoController from './VideoController.js';
 
+// Must be global so that it can be used in Youtube API event listener
 var streamModeState = {
-    isStreamModeEnabled: false
+    isStreamModeEnabled: false,
+    streamModeTimer: null
 };
-
-$(document).ready(function(){
-    initializeChangeModeSelectEventListener();
-});
 
 export function initializeChangeModeSelectEventListener() {
     changeModeElements.changeModeSelect.addEventListener('change', function(e) {
@@ -19,18 +17,41 @@ export function initializeChangeModeSelectEventListener() {
         if(selectionValue === 'default') {
             // unhide all annotations
             for(let i = 0; i < annotationElements.length; i++) {
-                annotationElements[i].classList.remove('hidden');
+                let annotationElement = annotationElements[i];
+                $(annotationElement).fadeIn();
             }
+            
+            // Clear streamModeTimer
+            clearInterval(streamModeState.streamModeTimer);
         } else if (selectionValue === 'stream') {
             // hide all annotations that have timestamp greater than current timestamp
             for(let i = 0; i < annotationElements.length; i++) {
                 let annotationElement = annotationElements[i];
                 let annotationTimestamp = videoController.getAnnotationTimestamp(annotationElement);
-                
+
                 if(annotationTimestamp > currentTimestamp){
-                    annotationElement.classList.add('hidden');
+                    $(annotationElement).fadeOut();
                 }
             }
+            
+            // start interval that will unhide annotations at their timestamps
+            streamModeState.streamModeTimer = setInterval(function() { 
+                handleAnnotationsDisplay(annotationElements, videoController.getVideoTimestamp())
+            }, 500);
         }
     });
+}
+
+function handleAnnotationsDisplay(annotationElements, currentVideoTimestamp) {
+    for(let i = 0; i < annotationElements.length; i++) {
+        let annotationElement = annotationElements[i];
+        let annotationTimestamp = videoController.getAnnotationTimestamp(annotationElement);
+
+        if(annotationTimestamp <= currentVideoTimestamp){
+            $(annotationElement).fadeIn();
+        } else if (annotationTimestamp > currentVideoTimestamp) {
+            $(annotationElement).fadeOut();
+        }
+    }
+    console.log('running');
 }
