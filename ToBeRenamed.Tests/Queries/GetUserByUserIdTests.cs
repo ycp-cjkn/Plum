ac325cc183a20698f66d955659d2587dcb75a383
Nate <nrussell@ycp.edu>
2018-12-03 23:21:23 -0500

    Some progress on GetUserByUserIdTest

---
+++ ToBeRenamed.Tests/Queries/GetUserByUserIdTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ToBeRenamed.Commands;
+using ToBeRenamed.Queries;
+using Xunit;
+
+namespace ToBeRenamed.Tests.Queries
+{
+    public class GetUserByUserIdTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _fixture;
+
+        public GetUserByUserIdTests(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task It_Gets_User_By_UserId()
+        {
+
+            var request = new CreateUserWithoutAuth("Alice");
+            var result = await _fixture.SendAsync(request);
+
+            var userId = result.Id;
+            var request1 = new GetUserByUserId(userId);
+            var result1 = await _fixture.SendAsync(request1);
+
+            Assert.Equal("Alice", result1.DisplayName);
+            Assert.Equal(1, result1.Id);
+        }
+    }
+}
