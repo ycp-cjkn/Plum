6572936fa73c5fa2042891bddb80c1ee4d3afcb2
Nathaniel Russell <nrussell@ycp.edu>
2018-12-05 14:10:03 -0500

    Completed UpdateDisplayNameTests

--- ToBeRenamed.Tests/Commands/UpdateDisplayNameTests.cs
+++ ToBeRenamed.Tests/Commands/UpdateDisplayNameTests.cs
@@ -18,20 +18,36 @@ namespace ToBeRenamed.Tests.Commands
         [ResetDatabase]
         public async Task It_Updates_A_DisplayName()
         {
-            //create user named Alice
-            var request = new CreateUserWithoutAuth("Alice");
-            var result = await _fixture.SendAsync(request);
+            const string title = "My Fantastic Library";
+            const string description = "A suitable description.";
 
-            //change name Alice of resultId to Bob
-            var request1 = new UpdateDisplayName(result.Id, "Bob");
-            var result2 = await _fixture.SendAsync(request1);
+            // Create a test user (don't think this is needed here)
+            var userRequest = new CreateUserWithoutAuth("Alice");
+            var user = await _fixture.SendAsync(userRequest);
 
-            //retrieve user info through same id 
-            var request2 = new GetUserByUserId(result.Id);
-            var result1 = await _fixture.SendAsync(request2);
+            // Create a library with that user
+            var request = new CreateLibrary(user.Id, title, description);
+            await _fixture.SendAsync(request);
+
+            // Get library created by that user
+            var librariesRequest = new GetLibrariesForUser(user.Id);
+            var library = (await _fixture.SendAsync(librariesRequest)).ToArray();
+            var libraryId = library.Single().Id;
+
+            //Return that member
+            var getMemberRequest = new GetMembersOfLibrary(libraryId);
+            var member = await _fixture.SendAsync(getMemberRequest);
+
+            //Update name from Alice to Bob 
+            var updateDisplayNameRequest = new UpdateDisplayName(member.Single().Id, "Bob");
+            await _fixture.SendAsync(updateDisplayNameRequest);
+
+            //Return same member with changed display name
+            var getMemberRequest1 = new GetMembersOfLibrary(member.Single().Id);
+            var member1 = await _fixture.SendAsync(getMemberRequest1);
 
             //Bob should be new name 
-            Assert.Equal("Bob", result1.DisplayName);
+            Assert.Equal("Bob", member1.Single().DisplayName);
         }
     }
 }
