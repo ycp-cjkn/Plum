9985882f5ddbb8b782938ff583d0f3216cc75a21
Jason Lefever <jlefever@ycp.edu>
2018-12-13 00:48:04 -0500

    Implement a search bar for libraries and videos

---
+++ ToBeRenamed/Models/SearchResults.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ToBeRenamed.Dtos;
+
+namespace ToBeRenamed.Models
+{
+    public class SearchResults
+    {
+        public IList<LibraryDto> Libraries { get; }
+        public IList<VideoDto> Videos { get; }
+
+        public int TotalCount => Libraries.Count + Videos.Count;
+
+        public SearchResults(IList<LibraryDto> libraries, IList<VideoDto> videos)
+        {
+            Libraries = libraries;
+            Videos = videos;
+        }
+    }
+}
